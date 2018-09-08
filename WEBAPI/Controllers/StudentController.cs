601b3aa3449ebca2a832d0209a1e4e38040e34cc
Jamal Abbasi <jamalabbasi1997@gmail.com>
2018-09-08 14:08:12 +0500

    WebApi Implemented in 3 tier

---
+++ WEBAPI/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Repository.DomainModel;
+using Repository.Repositories;
+
+namespace WEBAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+
+        [HttpGet]
+        [Route("api/Student/{id}")]
+        public StudentModel Get(int id)
+        {
+            try
+            {
+                var _studentRepository = new StudentRepository();
+                StudentModel student = _studentRepository.LoadStudentData(id);
+                if (student == null) return null;
+                return student;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
